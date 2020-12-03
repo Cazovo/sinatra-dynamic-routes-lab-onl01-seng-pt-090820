a55f8f9b07f611ba868df4068fa4577963c131ed
conner fortner <fortner.conner@yahoo.com>
2020-12-03 20:52:45 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,6 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  # Write your code here!
+ get '/:operation/:number1/:number2' do
 
-end
+    @operation = params[:operation]
+    @num1 = params[:number1].to_i
+    @num2 = params[:number2].to_i
+
+    case @operation
+    when "subtract"
+      (@num1 - @num2).to_s
+    when "add"
+      (@num1 + @num2).to_s
+    when "divide"
+      (@num1 / @num2).to_s
+    when "multiply"
+      (@num1 * @num2).to_s
+    end 
